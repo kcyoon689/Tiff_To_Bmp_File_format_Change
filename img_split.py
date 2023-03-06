03c76a9a4d31d98555205ff68ca78dcf157add28
ChaeYoonKim <kcyoun689@gmail.com>
2023-03-06 19:36:34 +0900

    resize and format change

---
+++ img_split.py
@@ -0,0 +1,45 @@
+# sudo apt install libgdal-dev
+# pip install gdal==3.4.1
+
+import os
+import cv2
+import numpy as np
+from osgeo import gdal
+from tqdm import tqdm
+
+IMG_PATH = 'img/'
+IMG_RESIZE_PATH = 'img_resize/'
+
+img_file_list = []
+for node_name in os.listdir(IMG_PATH):
+    node_path = os.path.join(IMG_PATH, node_name)
+    if os.path.isfile(node_path):
+        img_file_list.append(node_name)
+        
+if '.DS_Store' in img_file_list:
+    img_file_list.remove('.DS_Store')
+
+print(img_file_list)
+
+pbar = tqdm(img_file_list)
+
+for img_file in pbar:
+    source = gdal.Open(IMG_PATH + img_file)
+
+    red = source.GetRasterBand(1).ReadAsArray()
+    green = source.GetRasterBand(2).ReadAsArray()
+    blue = source.GetRasterBand(3).ReadAsArray()
+
+    img_np = np.dstack((red, green, blue))
+    # print(img_np.shape)
+
+    resize_scale = 10
+    new_size = (int(img_np.shape[0]/resize_scale),
+                int(img_np.shape[1]/resize_scale))
+    # print(new_size)
+
+    img_resize_np = cv2.resize(img_np, new_size)
+    # print(img_resize_np.shape)
+
+    cv2.imwrite(IMG_RESIZE_PATH + img_file.split('.')
+                [0] + '_resize_' + str(resize_scale) + '.bmp', img_resize_np)
